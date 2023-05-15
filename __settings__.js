070be0a8d6c53af88a3fe0a6f722f0a3b529aa9c
BioCryoGenesis <113868401+BioCryoGenesis@users.noreply.github.com>
2023-05-14 22:10:43 -0500

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1746100.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 134041526, 134041534, 134041536, 134041550, 134041553, 134041560, 134041562, 134041563 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/134041523/1/basis.wasm.js', 'wasmUrl' : 'files/assets/134041524/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/134041522/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/134041519/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/134041520/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/134041518/1/ammo.js', 'preload' : true},
+];
